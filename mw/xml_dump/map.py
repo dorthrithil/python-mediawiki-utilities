de283c5d89aaa533a44c46c8063b8f35ed0d2fcd
Felix Engelmann <felix@feblog.de>
2016-05-04 11:34:34 +0200

    Fix NotImplementedError on Mac OS X where sem_getvalue() is not implemented by subclassing multiprocessing.queues.Queue and using a SharedCounter for tracking the queue size

--- mw/xml_dump/map.py
+++ mw/xml_dump/map.py
@@ -1,9 +1,70 @@
 import logging
-from multiprocessing import Queue, Value, cpu_count
+import multiprocessing
+import multiprocessing.queues
 from queue import Empty
 
 from .functions import file
 from .processor import DONE, Processor
+
+
+class SharedCounter(object):
+    """ A synchronized shared counter.
+    The locking done by multiprocessing.Value ensures that only a single
+    process or thread may read or write the in-memory ctypes object. However,
+    in order to do n += 1, Python performs a read followed by a write, so a
+    second process may read the old value before the new one is written by the
+    first process. The solution is to use a multiprocessing.Lock to guarantee
+    the atomicity of the modifications to Value.
+    This class comes almost entirely from Eli Bendersky's blog:
+    http://eli.thegreenplace.net/2012/01/04/shared-counter-with-pythons-multiprocessing/
+    """
+
+    def __init__(self, n=0):
+        self.count = multiprocessing.Value('i', n)
+
+    def increment(self, n=1):
+        """ Increment the counter by n (default = 1) """
+        with self.count.get_lock():
+            self.count.value += n
+
+    @property
+    def value(self):
+        """ Return the value of the counter """
+        return self.count.value
+
+
+class Queue(multiprocessing.queues.Queue):
+    """ A portable implementation of multiprocessing.Queue.
+    Because of multithreading / multiprocessing semantics, Queue.qsize() may
+    raise the NotImplementedError exception on Unix platforms like Mac OS X
+    where sem_getvalue() is not implemented. This subclass addresses this
+    problem by using a synchronized shared counter (initialized to zero) and
+    increasing / decreasing its value every time the put() and get() methods
+    are called, respectively. This not only prevents NotImplementedError from
+    being raised, but also allows us to implement a reliable version of both
+    qsize() and empty().
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Queue, self).__init__(ctx=multiprocessing, *args, **kwargs)
+        self.size = SharedCounter(0)
+
+    def put(self, *args, **kwargs):
+        self.size.increment(1)
+        super(Queue, self).put(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        self.size.increment(-1)
+        return super(Queue, self).get(*args, **kwargs)
+
+    def qsize(self):
+        """ Reliable implementation of multiprocessing.Queue.qsize() """
+        return self.size.value
+
+    def empty(self):
+        """ Reliable implementation of multiprocessing.Queue.empty() """
+        return not self.qsize()
+
 
 logger = logging.getLogger("mw.xml_dump.map")
 
@@ -14,7 +75,7 @@ def re_raise(error, path):
 
 
 def map(paths, process_dump, handle_error=re_raise,
-        threads=cpu_count(), output_buffer=100):
+        threads=multiprocessing.cpu_count(), output_buffer=100):
     """
     Maps a function across a set of dump files and returns
     an (order not guaranteed) iterator over the output.
@@ -55,7 +116,7 @@ def map(paths, process_dump, handle_error=re_raise,
     paths = list(paths)
     pathsq = queue_files(paths)
     outputq = Queue(maxsize=output_buffer)
-    running = Value('i', 0)
+    running = multiprocessing.Value('i', 0)
     threads = max(1, min(int(threads), pathsq.qsize()))
 
     processors = []
